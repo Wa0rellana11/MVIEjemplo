ffc3562215411cfe0e12b242ecd9e71c07636f83
Wa0rellana11 <wa0rellana11@gmail.com>
2024-12-07 18:07:14 -0500

    agrego when en el viewModel

--- app/src/main/java/com/example/mviejemplo/presentacion/ClimaViewModel.kt
+++ app/src/main/java/com/example/mviejemplo/presentacion/ClimaViewModel.kt
@@ -8,7 +8,16 @@ class ClimaViewModel {
     var estado by mutableStateOf<ClimaEstado>(ClimaEstado.Vacio)
 
     fun ejecutar(intencion: ClimaIntencion){
+when(intencion){
+    ClimaIntencion.actualiza -> actualizar()
+}
+    }
 
+    private fun actualizar(){
+        estado = ClimaEstado.Cargando
+        //TODO conseguir los datos desde un repositorio
+        estado = ClimaEstado.Error("No funciono el servidor, esta todo roto")
+        
     }
 
 
